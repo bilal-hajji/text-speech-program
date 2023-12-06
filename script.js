0e3634f2e347c6f240053197c15313626ccfdb26
Bilal-Abdille <bilalmaalim57@gmail.com>
2023-12-06 12:09:56 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function showtime() {
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let session = "AM";
+
+    if (h == 0) {
+        h = 12; //12 hour format
+    }
+
+    if (h > 12) {
+        h = h - 12; //12 hour format
+        session = "PM"
+    }
+
+    // if (h < 10) {
+    //     h = "0" + h;  //0-9
+    // }
+
+    // if (m < 10) {
+    //     m = "0" + m;
+    // }
+
+    // if (s < 10) {
+    //     s = "0" + s;
+    // }
+
+    h=(h<10)?"0"+h:h;
+    m=(m<10)?"0"+m:m;
+    s=(s<10)?"0"+s:s;
+
+
+
+    document.getElementById("digitalclock").innerHTML = h + ":" + m + ":" + s + "" + session;
+
+    setTimeout(showtime, 1000);
+}
+showtime();
